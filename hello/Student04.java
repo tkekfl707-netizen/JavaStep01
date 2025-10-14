b873bc32dfccb7df28976d8a6f4ca8db8f0d68b0
tkekfl707-netizen <tkekfl707@gmail.com>
2025-10-14 13:19:19 +0900

    Add files via upload

--- hello/Student04.java
+++ hello/Student04.java
@@ -5,13 +5,14 @@ public class Student04 {//참조 자료형 변수의 인스턴스 생성하기
 	String studentName; //String(참조 자료형)
 	Subject01 korean; // 참초 자료형(Subject01)
 	Subject01 math; // 참초 자료형(Subject01)
+	// 객체의 주소값을 저장하고, 그 객체를 통해 점수와 과목명을 저장/조회 가능
 	
 	public Student04(int studentID, String studentName) {
-		this.studentID = studentID;
-		this.studentName = studentName;
+		this.studentID = studentID; // 현재 객체의 studentID에 매개변수 값을 대입
+		this.studentName = studentName; // 현재 객체의 studentName에 매개변수 값을 대입
 		
-		korean = new Subject01();
-		math = new Subject01();
+		korean = new Subject01(); //중요: 여기서 korean = new Subject01(); -> Subject01 객체를 새로 생성
+		math = new Subject01();   //-> 각 학생마다 독립적인 과목 객체를 갖게됨
 	}
 	
 	public void showStudentInfo() {
